958da2db0fc71ecdc3fdb1898dba8d252161f827
Darron Froese <darron@froese.org>
2015-11-05 16:39:45 -0700

    Nicer and cleaner.

--- request.go
+++ request.go
@@ -22,10 +22,8 @@ import (
 // uriForAPI is to be called with something like "/v1/events" and it will give
 // the proper request URI to be posted to.
 func (self *Client) uriForAPI(api string) string {
-	var url = ""
-	if os.Getenv("DATADOG_HOST") != "" {
-		url = os.Getenv("DATADOG_HOST")
-	} else {
+	url := os.Getenv("DATADOG_HOST")
+	if url == "" {
 		url = "https://app.datadoghq.com"
 	}
 	if strings.Index(api, "?") > -1 {
