aa88eb2b7e79fa16ff994562c10818de2da5a854
Simarpreet Singh <simar@linux.com>
2017-10-23 23:15:15 -0700

    hosts: Add Host Muting/Unmuting capability
    This implements the following:
    Muting a host: https://docs.datadoghq.com/api/?lang=console#hosts-mute
    Unmuting a host: https://docs.datadoghq.com/api/?lang=console#hosts-unmute
    
    Included with this commit is an integration test for the API.
    
    Signed-off-by: Simarpreet Singh <simar@linux.com>

---
+++ integration/hosts_test.go
@@ -0,0 +1,45 @@
+package integration
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	datadog "github.com/zorkian/go-datadog-api"
+)
+
+func init() {
+	client = initTest()
+}
+
+func TestHost_Mute(t *testing.T) {
+	muteAction := getTestMuteAction()
+	hostname := "test.host"
+	muteResp, err := client.MuteHost(hostname, muteAction)
+	if err != nil {
+		t.Fatalf("Failed to mute host: %s, err: %s", hostname, err)
+	}
+
+	defer func() {
+		unmuteResp, err := client.UnmuteHost(hostname)
+		if err != nil {
+			t.Fatalf("Failed to cleanup mute on host: %s, err: %s", hostname, err)
+		}
+		assert.Equal(t, "Unmuted", unmuteResp.Action)
+		assert.Equal(t, hostname, unmuteResp.Hostname)
+	}()
+
+	assert.Equal(t, "Muted", muteResp.Action)
+	assert.Equal(t, hostname, muteResp.Hostname)
+	assert.Equal(t, "Muting this host for a test!", muteResp.Message)
+
+}
+
+func getTestMuteAction() *datadog.HostActionMute {
+	return &datadog.HostActionMute{
+		Message:  datadog.String("Muting this host for a test!"),
+		EndTime:  datadog.String(fmt.Sprint(time.Now().Unix() + 100)),
+		Override: datadog.Bool(false),
+	}
+}
