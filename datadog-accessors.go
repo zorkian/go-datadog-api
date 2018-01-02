37dbd386e13c75cbc9ae41493717e490bb9a3c39
Mark Smith <mark@dreamwidth.org>
2018-01-01 16:00:40 -0800

    Fix accessor generation for 2018

--- datadog-accessors.go
+++ datadog-accessors.go
@@ -4,7 +4,7 @@
  *
  * Please see the included LICENSE file for licensing information.
  *
- * Copyright 2017 by authors and contributors.
+ * Copyright 2018 by authors and contributors.
 */
 
 package datadog
