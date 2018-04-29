6c08e2322af96e867e5715aedd6ea194c42cf44f
Otto Jongerius <otto@vol.community>
2018-04-30 09:58:32 +1200

    Revert "Change DataPoint to array of pointers (#146)"
    This reverts commit e105a8d59e751cc42d6e7aed2eab4bc9a38aabeb.

--- series.go
+++ series.go
@@ -15,7 +15,7 @@ import (
 
 // DataPoint is a tuple of [UNIX timestamp, value]. This has to use floats
 // because the value could be non-integer.
-type DataPoint [2]*float64
+type DataPoint [2]float64
 
 // Metric represents a collection of data points that we might send or receive
 // on one single metric line.
