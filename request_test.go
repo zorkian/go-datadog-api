5282e9fe42d850176e8e2ac342cee5ef92a409e2
Anton Tolchanov <knyar@google.com>
2018-09-19 11:36:13 +0100

    Propagate errors from JSON response
    This fixes #170.

--- request_test.go
+++ request_test.go
@@ -3,6 +3,7 @@ package datadog
 import (
 	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -58,5 +59,59 @@ func TestRedactError(t *testing.T) {
 		var redactedErr = c.redactError(harmlessErr)
 
 		assert.Nil(t, redactedErr)
+	})
+}
+
+func makeTestServer(code int, response string) *httptest.Server {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api/v1/something", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(code)
+		w.Write([]byte(response))
+	})
+	server := httptest.NewServer(mux)
+	return server
+}
+
+func TestErrorHandling(t *testing.T) {
+	c := Client{
+		apiKey:       "sample_api_key",
+		appKey:       "sample_app_key",
+		HttpClient:   &http.Client{},
+		RetryTimeout: 1000,
+	}
+	for _, code := range []int{401, 403, 500, 502} {
+		t.Run(fmt.Sprintf("Returns error on http code %d", code), func(t *testing.T) {
+			s := makeTestServer(code, "")
+			defer s.Close()
+			c.SetBaseUrl(s.URL)
+
+			for _, method := range []string{"GET", "POST", "PUT"} {
+				err := c.doJsonRequest(method, "/v1/something", nil, nil)
+				assert.NotNil(t, err)
+			}
+		})
+	}
+	t.Run("Returns error if status is error", func(t *testing.T) {
+		s := makeTestServer(200, `{"status": "error", "error": "something wrong"}`)
+		defer s.Close()
+		c.SetBaseUrl(s.URL)
+
+		for _, method := range []string{"GET", "POST", "PUT"} {
+			err := c.doJsonRequest(method, "/v1/something", nil, nil)
+			if assert.NotNil(t, err) {
+				assert.Contains(t, err.Error(), "something wrong")
+			}
+		}
+	})
+	t.Run("Does not return error if status is ok", func(t *testing.T) {
+		s := makeTestServer(200, `{"status": "ok"}`)
+		defer s.Close()
+		c.SetBaseUrl(s.URL)
+
+		for _, method := range []string{"GET", "POST", "PUT"} {
+			err := c.doJsonRequest(method, "/v1/something", nil, nil)
+			assert.Nil(t, err)
+		}
 	})
 }
