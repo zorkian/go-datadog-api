c1bfd47c56f05b6a0841981e768aaba38bd20fcb
dharada1 <haradadaichi715@gmail.com>
2019-02-10 02:50:16 +0900

    fix struct name because it was conflicted with Service Check

--- synthetics.go
+++ synthetics.go
@@ -8,70 +8,70 @@
 
 package datadog
 
-type Check struct {
-	Status          *string         `json:"status,omitempty"`
-	PublicId        *string         `json:"public_id,omitempty"`
-	Tags            []*string       `json:"tags,omitempty"`
-	Locations       []*string       `json:"locations,omitempty"`
-	Notifications   []*Notification `json:"notifications,omitempty"`
-	CheckStatus     *string         `json:"check_status,omitempty"`
-	Message         *string         `json:"message,omitempty"`
-	Id              *int            `json:"id,omitempty"`
-	LastTriggeredTs *int            `json:"last_triggered_ts,omitempty"`
-	Name            *string         `json:"string,omitempty"`
-	MonitorId       *int            `json:"monitor_id,omitempty"`
-	Type            *string         `json:"api,omitempty"`
-	CreatedAt       *string         `json:"created_at,omitempty"`
-	ModifiedAt      *string         `json:"modified_at,omitempty"`
-	Config          []*Config       `json:"config,omitempty"`
-	Options         *Options        `json:"options,omitempty"`
+type SyntheticsCheck struct {
+	Status          *string                       `json:"status,omitempty"`
+	PublicId        *string                       `json:"public_id,omitempty"`
+	Tags            []string                      `json:"tags,omitempty"`
+	Locations       []string                      `json:"locations,omitempty"`
+	Notifications   []SyntheticsCheckNotification `json:"notifications,omitempty"`
+	CheckStatus     *string                       `json:"check_status,omitempty"`
+	Message         *string                       `json:"message,omitempty"`
+	Id              *int                          `json:"id,omitempty"`
+	LastTriggeredTs *int                          `json:"last_triggered_ts,omitempty"`
+	Name            *string                       `json:"string,omitempty"`
+	MonitorId       *int                          `json:"monitor_id,omitempty"`
+	Type            *string                       `json:"api,omitempty"`
+	CreatedAt       *string                       `json:"created_at,omitempty"`
+	ModifiedAt      *string                       `json:"modified_at,omitempty"`
+	Config          []SyntheticsCheckConfig       `json:"config,omitempty"`
+	Options         *SyntheticsCheckOptions       `json:"options,omitempty"`
 }
 
-type Notification struct {
+type SyntheticsCheckNotification struct {
 	Handle  *string `json:"handle,omitempty"`
 	Name    *string `json:"name,omitempty"`
 	Service *string `json:"service,omitempty"`
 	Icon    *string `json:"icon,omitempty"`
 }
 
-type Config struct {
-	Request    *Request     `json:"request,omitempty"`
-	Assertions []*Assertion `json:"assertions,omitempty"`
-	Locations  *string      `json:"locations,omitempty"`
+type SyntheticsCheckConfig struct {
+	Request    *SyntheticsCheckRequest    `json:"request,omitempty"`
+	Assertions []SyntheticsCheckAssertion `json:"assertions,omitempty"`
+	Locations  *string                    `json:"locations,omitempty"`
 }
 
-type Request struct {
+type SyntheticsCheckRequest struct {
 	Url     *string `json:"url,omitempty"`
 	Method  *string `json:"method,omitempty"`
 	Timeout *int    `json:"timeout,omitempty"`
 }
 
-type Assertion struct {
+type SyntheticsCheckAssertion struct {
 	Operator *string `json:"operator,omitempty"`
 	Property *string `json:"property,omitempty"`
 	Type     *string `json:"type,omitempty"`
 	Target   *string `json:"target,omitempty"`
 }
 
-type Options struct {
+type SyntheticsCheckOptions struct {
 	TickEvery *int `json:"tick_every,omitempty"`
 }
 
 // /api/v0/synthetics/checks/search
 type reqSearchChecks struct {
-	Checks []*Check `json:"screenboards,omitempty"`
+	Checks []SyntheticsCheck `json:"screenboards,omitempty"`
 }
 
-func (client *Client) CreateChecks(text string) ([]Check, error) {
-	var out reqSearch
+func (client *Client) CreateSyntheticsChecks(text string) ([]SyntheticsCheck, error) {
+	var out reqSearchChecks
 	if err := client.doJsonRequest("GET", "/v0/synthetics/checks/search?text="+text, nil, &out); err != nil {
 		return nil, err
 	}
 	return out.Checks, nil
 }
 
-func (client *Client) CreateCheck(check *Check) (*Check, error) {
-	var out Check
+func (client *Client) CreateSyntheticsCheck(check *SyntheticsCheck) (*SyntheticsCheck, error) {
+	var out SyntheticsCheck
 	if err := client.doJsonRequest("POST", "/v0/synthetics/checks", check, &out); err != nil {
 		return nil, err
 	}
