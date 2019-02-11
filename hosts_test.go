bb89771067fdbf77094712a5adcbe43d8196e67d
dharada1 <haradadaichi715@gmail.com>
2019-02-11 13:13:35 +0900

    Add Unit Test

---
+++ hosts_test.go
@@ -0,0 +1,34 @@
+package datadog
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetHostTotals(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		response, err := ioutil.ReadFile("./tests/fixtures/hosts/get_totals_response.json")
+		if err != nil {
+			t.Fatal(err)
+		}
+		w.Write(response)
+	}))
+	defer ts.Close()
+
+	datadogClient := Client{
+		baseUrl:    ts.URL,
+		HttpClient: http.DefaultClient,
+	}
+
+	res, err := datadogClient.GetHostTotals()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, res.TotalActive, 1)
+	assert.Equal(t, res.TotalUp, 2)
+}
