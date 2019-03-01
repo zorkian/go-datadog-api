02a01a83d67e922b94801f38d76dee6a326efbe1
Willi Schinmeyer <willi.schinmeyer@freiheit.com>
2019-03-01 14:20:12 +0100

    Improve Screen- and Dashboard tests
    - Use Client of httptest Server
    - Set RetryTimeout in datadogClient so it doesn't retry indefinitely on error
    - Move testdata into the well-known testdata/ folder so test caching etc. works
      properly
    - Return error from mock Datadog server instead of calling t.Fatal, which is
      only allowed in the test's main Goroutine
    - Use reflect.DeepEqual to shorten Screenboard test

--- screenboards_test.go
+++ screenboards_test.go
@@ -4,92 +4,56 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 func TestGetScreenboard(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		response, err := ioutil.ReadFile("./tests/fixtures/screenboard_response.json")
+		response, err := ioutil.ReadFile("./testdata/fixtures/screenboard_response.json")
 		if err != nil {
-			t.Fatal(err)
+			// mustn't call t.Fatal from a different Goroutine
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 		w.Write(response)
 	}))
 	defer ts.Close()
 
 	datadogClient := Client{
-		baseUrl:    ts.URL,
-		HttpClient: http.DefaultClient,
+		baseUrl:      ts.URL,
+		HttpClient:   ts.Client(),
+		RetryTimeout: 5 * time.Second,
 	}
 
-	screenboard, err := datadogClient.GetScreenboard(6334)
+	got, err := datadogClient.GetScreenboard(6334)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	expectedID := 6334
-	if id := screenboard.GetId(); id != expectedID {
-		t.Fatalf("expect ID %d. Got %d", expectedID, id)
+	want := &Screenboard{
+		Id:       Int(6334),
+		Title:    String("dogapi test"),
+		Height:   Int(768),
+		Width:    Int(1024),
+		ReadOnly: Bool(false),
+		Widgets: []Widget{
+			{
+				Type:   String("image"),
+				Height: Int(20),
+				Width:  Int(32),
+				X:      Int(32),
+				Y:      Int(7),
+				URL:    String("http://path/to/image.jpg"),
+			},
+		},
 	}
 
-	expectedTitle := "dogapi test"
-	if title := screenboard.GetTitle(); title != expectedTitle {
-		t.Fatalf("expect title %s. Got %s", expectedTitle, title)
-	}
-
-	expectedHeight := 768
-	if height := screenboard.GetHeight(); height != expectedHeight {
-		t.Fatalf("expect height %d. Got %d", expectedHeight, height)
-	}
-
-	expectedWidth := 1024
-	if width := screenboard.GetWidth(); width != expectedWidth {
-		t.Fatalf("expect width %d. Got %d", expectedWidth, width)
-	}
-
-	expectedReadOnly := false
-	readOnly, ok := screenboard.GetReadOnlyOk()
-	if !ok {
-		t.Fatalf("expect to have a read_only field")
-	}
-
-	if readOnly != expectedReadOnly {
-		t.Fatalf("expect read_only %v. Got %v", expectedReadOnly, readOnly)
-	}
-
-	for _, widget := range screenboard.Widgets {
-		validateWidget(t, widget)
-	}
-}
-
-func validateWidget(t *testing.T, wd Widget) {
-	expectedType := "image"
-	if widgetType := wd.GetType(); widgetType != expectedType {
-		t.Fatalf("expect type %s. Got %s", expectedType, widgetType)
-	}
-
-	expectedHeight := 20
-	if height := wd.GetHeight(); height != expectedHeight {
-		t.Fatalf("expect height %d. Got %d", expectedHeight, height)
-	}
-
-	expectedWidth := 32
-	if width := wd.GetWidth(); width != expectedWidth {
-		t.Fatalf("expect width %d. Got %d", expectedWidth, width)
-	}
-
-	expectedX := 32
-	if x := wd.GetX(); x != expectedX {
-		t.Fatalf("expect x %d. Got %d", expectedX, x)
-	}
-
-	expectedY := 7
-	if y := wd.GetY(); y != expectedY {
-		t.Fatalf("expect y %d. Got %d", expectedY, y)
-	}
-
-	expectedURL := "http://path/to/image.jpg"
-	if url := wd.GetURL(); url != expectedURL {
-		t.Fatalf("expect url %s. Got %s", expectedURL, url)
+	if !reflect.DeepEqual(got, want) {
+		// Go formatting doesn't handle pointers gracefully, so we're spewing
+		t.Errorf("got:\n%s\nwant:\n%s", spew.Sdump(got), spew.Sdump(want))
 	}
 }
