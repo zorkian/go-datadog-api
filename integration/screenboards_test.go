ae055507aac49f9fb011ad17b86de6ec3ed836f5
Rami Enbashi <rami@enbashi.com>
2019-04-15 09:59:52 -0400

    [dashboards] Support getting dashboards by string id (#225)
    [dashboards] Support getting dashboards by string id

--- integration/screenboards_test.go
+++ integration/screenboards_test.go
@@ -3,6 +3,7 @@ package integration
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/zorkian/go-datadog-api"
 )
 
@@ -88,6 +89,60 @@ func TestScreenboardGet(t *testing.T) {
 	if num+1 != len(boards) {
 		t.Fatalf("Number of screenboards didn't match expected: %d != %d", len(boards), num+1)
 	}
+}
+
+func TestScreenboardGetWithNewId(t *testing.T) {
+	expected := getTestScreenboard()
+	// create the screenboard and compare it
+	actual, err := client.CreateScreenboard(expected)
+	if err != nil {
+		t.Fatalf("Creating a screenboard failed when it shouldn't. (%s)", err)
+	}
+
+	defer cleanUpScreenboard(t, *actual.Id)
+
+	assertScreenboardEquals(t, actual, expected)
+
+	// try to fetch it freshly and compare it again
+	actual, err = client.GetScreenboard(*actual.Id)
+	if err != nil {
+		t.Fatalf("Retrieving a screenboard failed when it shouldn't. (%s)", err)
+	}
+
+	assertScreenboardEquals(t, actual, expected)
+
+	// try to fetch it freshly using the new id format and compare it again
+	actualWithNewId, err := client.GetScreenboard(*actual.NewId)
+	if err != nil {
+		t.Fatalf("Retrieving a screenboard failed when it shouldn't. (%s)", err)
+	}
+	assertScreenboardEquals(t, actualWithNewId, expected)
+
+	// the ids are equal whether fetching using the old or the new id
+	assert.Equal(t, *actualWithNewId.Id, *actual.Id)
+
+	// try to fetch it freshly using a string, but with a wrong value
+	actual, err = client.GetScreenboard("random_string")
+	if assert.NotNil(t, err) {
+		// it should not fail because of the id format
+		assert.NotContains(t, err.Error(), "unsupported id type")
+		assert.Contains(t, err.Error(), "404")
+	}
+
+	// try to fetch it freshly using a boolean
+	actual, err = client.GetScreenboard(true)
+	if assert.NotNil(t, err) {
+		// it should fail because of the id format
+		assert.Contains(t, err.Error(), "unsupported id type")
+	}
+
+	// try to fetch it freshly using a float64
+	actual, err = client.GetScreenboard(5.5)
+	if assert.NotNil(t, err) {
+		// it should fail because of the id format
+		assert.Contains(t, err.Error(), "unsupported id type")
+	}
+
 }
 
 func getTestScreenboard() *datadog.Screenboard {
