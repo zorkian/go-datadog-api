8665c25fff7e7f63bc34766c0bceb254918c46f4
David Bouchare <david.bouchare@datadoghq.com>
2019-08-22 15:28:00 +0200

    Add total endpoint

--- hosts.go
+++ hosts.go
@@ -1,9 +1,9 @@
 package datadog
 
 type HostActionResp struct {
-	Action   string `json:"action"`
-	Hostname string `json:"hostname"`
-	Message  string `json:"message,omitempty"`
+	Action   *string `json:"action"`
+	Hostname *string `json:"hostname"`
+	Message  *string `json:"message,omitempty"`
 }
 
 type HostActionMute struct {
@@ -27,6 +27,23 @@ func (client *Client) UnmuteHost(host string) (*HostActionResp, error) {
 	var out HostActionResp
 	uri := "/v1/host/" + host + "/unmute"
 	if err := client.doJsonRequest("POST", uri, nil, &out); err != nil {
+		return nil, err
+	}
+	return &out, nil
+}
+
+// HostTotalsResp defines response to GET /v1/hosts/totals.
+type HostTotalsResp struct {
+	TotalUp     *int `json:"total_up"`
+	TotalActive *int `json:"total_active"`
+}
+
+// GetHostTotals returns number of total active hosts and total up hosts.
+// Active means the host has reported in the past hour, and up means it has reported in the past two hours.
+func (client *Client) GetHostTotals() (*HostTotalsResp, error) {
+	var out HostTotalsResp
+	uri := "/v1/hosts/totals"
+	if err := client.doJsonRequest("GET", uri, nil, &out); err != nil {
 		return nil, err
 	}
 	return &out, nil
