d26fa36fa70527c15d8e900d4a86434c535b1d6e
Elijah Oyekunle <eloyekunle@gmail.com>
2020-04-05 17:56:54 +0100

    use the new hosts search endpoint

--- hosts_test.go
+++ hosts_test.go
@@ -32,3 +32,34 @@ func TestGetHostTotals(t *testing.T) {
 	assert.Equal(t, *res.TotalActive, 1)
 	assert.Equal(t, *res.TotalUp, 2)
 }
+
+func TestGetHosts(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		response, err := ioutil.ReadFile("./tests/fixtures/hosts/get_hosts_response.json")
+		if err != nil {
+			t.Fatal(err)
+		}
+		w.Write(response)
+	}))
+	defer ts.Close()
+
+	datadogClient := Client{
+		baseUrl:    ts.URL,
+		HttpClient: http.DefaultClient,
+	}
+
+	searchRequest := HostSearchRequest{
+		SortField:     "cpu",
+		SortDirection: "desc",
+		Start:         0,
+		Count:         100,
+	}
+	res, err := datadogClient.GetHosts(searchRequest)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, res.TotalReturned, 3)
+	assert.Equal(t, res.TotalMatching, 3)
+	assert.Len(t, res.HostList, 3)
+}
